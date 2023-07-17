54e148218dd50eae4fe80f9e6c7f3a3b026865f7
klein <kleinjacob1738@gmail.com>
2023-07-17 17:13:04 +0300

    finalised lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,9 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      <h1>HELLO,world!</h1>
+      <p>This is my article.</p>
     </div>
   );
 }
-
 export default Article;
